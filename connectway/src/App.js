59e95bda88e2867a07cb580b489681d3a26bd42c
Faxterefalt <azugafabian321@gmail.com>
2024-10-04 13:55:09 -0400

    Audiolibros Registrados>EjemploGustavo

--- connectway/src/App.js
+++ connectway/src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/MiActividad" element={<MiActividad />} />
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Audiolibros/añadir" element={<AudiolibrosAñadir />} />
-        <Route path="/Audiolibros/registrados" element={<AudiolibrosRegistrado />} />
+        <Route path="/Audiolibros/registrados" element={<AudiobookList />} />
         <Route path="/ListaAudioLibros" element={<AudiobookList/>} />
         <Route path="/Formulario" element={<Formulario/>} /> 
       </Routes>
