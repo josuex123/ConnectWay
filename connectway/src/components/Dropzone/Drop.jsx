164be87e9e5e8556fac48c6afd2e2db51f58d6c1
JeremiasVA <eljerevizcarra@gmail.com>
2024-10-06 19:41:23 -0400

    ayuda en campos de form

--- connectway/src/components/Dropzone/Drop.jsx
+++ connectway/src/components/Dropzone/Drop.jsx
@@ -2,7 +2,10 @@ import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import './Drop.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage, faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faImage, faMusic, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Drop() {
@@ -56,81 +59,86 @@ function Drop() {
       noClick: true
     });
   
-return (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          {showModal && (
-            <div className="modal">
-              <div className="modal-content">
-                <h3>Error</h3>
-                <p>{audioError}</p>
-                <button onClick={closeModal}>Cerrar</button>
+    return (
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* Modal de Bootstrap */}
+        <Modal show={showModal} onHide={closeModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Error</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <FontAwesomeIcon icon={faExclamationCircle} size="4x" style={{ color: '#ffc107' }} />
+            <p className="mt-3">{audioError}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+  
+        {/* Zona de subida de imagen */}
+        <div className="dropzone-container">
+          <h3 className="dropzone-title">Subir portada:</h3>
+          <div {...imageDropzone.getRootProps()} className="dropzone">
+            {imageFiles.length === 0 && (
+              <>
+                <div className="icon-container">
+                  <FontAwesomeIcon icon={faImage} size="2x" className="icon" />
+                </div>
+                <button type="select-button" onClick={imageDropzone.open} className="select-button">
+                  Sube un archivo
+                </button>
+                <p>ó deslizalo aquí</p>
+              </>
+            )}
+            <input {...imageDropzone.getInputProps()} style={{ display: 'none' }} />
+            {imageFiles.length > 0 && (
+              <div className="uploaded-file">
+                <h4>Imagen Subida:</h4>
+                {imageFiles.map((file) => (
+                  <div key={file.path}>
+                    <img src={file.preview} alt={file.name} width="100px" />
+                    <p>{file.name}</p>
+                    <button onClick={removeImageFile}>Eliminar</button>
+                  </div>
+                ))}
               </div>
-            </div>
-          )}
-    
-          {/* Subtítulo para la portada */}
-          <div className="dropzone-container">
-            <h3 className="dropzone-title">Subir portada:</h3>
-            <div {...imageDropzone.getRootProps()} className="dropzone">
-              {imageFiles.length === 0 && (
-                <>
-                  <div className="icon-container">
-                    <FontAwesomeIcon icon={faImage} size="2x" className="icon" />
-                  </div>
-                  <button type="button" onClick={imageDropzone.open} className="select-button">
-                    Sube un archivo
-                  </button>
-                  <p>ó deslizalo aquí</p>
-                </>
-              )}
-              <input {...imageDropzone.getInputProps()} style={{ display: 'none' }} />
-              {imageFiles.length > 0 && (
-                <div className="uploaded-file">
-                  <h4>Imagen Subida:</h4>
-                  {imageFiles.map((file) => (
-                    <div key={file.path}>
-                      <img src={file.preview} alt={file.name} width="100px" />
-                      <p>{file.name}</p>
-                      <button onClick={removeImageFile}>Eliminar</button>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
-    
-          {/* Subtítulo para el audiolibro */}
-          <div className="dropzone-container">
-            <h3 className="dropzone-title">Subir audiolibro:</h3>
-            <div {...audioDropzone.getRootProps()} className="dropzone">
-              {audioFiles.length === 0 && (
-                <>
-                  <div className="icon-container">
-                    <FontAwesomeIcon icon={faMusic} size="2x" className="icon" />
-                  </div>
-                  <button type="button" onClick={audioDropzone.open} className="select-button">
-                    Sube un archivo
-                  </button>
-                  <p>ó deslizalo aquí</p>
-                </>
-              )}
-              <input {...audioDropzone.getInputProps()} style={{ display: 'none' }} />
-              {audioFiles.length > 0 && (
-                <div className="uploaded-file">
-                  <h4>Audio Subido:</h4>
-                  {audioFiles.map((file) => (
-                    <div key={file.path}>
-                      <p>{file.name}</p>
-                      <button onClick={removeAudioFile}>Eliminar</button>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
+            )}
           </div>
         </div>
-      );
-    }
   
+        {/* Zona de subida de audio */}
+        <div className="dropzone-container">
+          <h3 className="dropzone-title">Subir audiolibro:</h3>
+          <div {...audioDropzone.getRootProps()} className="dropzone">
+            {audioFiles.length === 0 && (
+              <>
+                <div className="icon-container">
+                  <FontAwesomeIcon icon={faMusic} size="2x" className="icon" />
+                </div>
+                <button type="select-button" onClick={audioDropzone.open} className="select-button">
+                  Sube un archivo
+                </button>
+                <p>ó deslizalo aquí</p>
+              </>
+            )}
+            <input {...audioDropzone.getInputProps()} style={{ display: 'none' }} />
+            {audioFiles.length > 0 && (
+              <div className="uploaded-file">
+                <h4>Audio Subido:</h4>
+                {audioFiles.map((file) => (
+                  <div key={file.path}>
+                    <p>{file.name}</p>
+                    <button onClick={removeAudioFile}>Eliminar</button>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-export default Drop
+export default Drop;
