406cb21176ba70c4ac7a0a0e5f4326f3f2375310
Josue <josue7pedro@gmail.com>
2024-10-05 23:53:02 -0400

    cambios en app

--- connectway/src/App.js
+++ connectway/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AudiobookList from "./Services/AudiolibrosServicios/AudiobooksList";
 import FormularioAñadir from './components/FormularioAñadir/Formulario';
 import Audiolibros from './pages/audiolibros/Audiolibros';
 import AudiolibrosRegistrado from './pages/audiolibros/AudiolibrosRegistrado';
@@ -23,7 +22,7 @@ function App() {
         <Route path="/MiActividad" element={<MiActividad />} />
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Audiolibros/añadir" element={<AudiolibrosAñadir />} />
-        <Route path="/Audiolibros/registrados" element={<AudiobookList />} />
+        <Route path="/Audiolibros/registrados" element={<AudiolibrosRegistrado />} />
         <Route path="/ListaAudioLibros" element={<AudiobookList/>} />
         <Route path="/Formulario" element={<FormularioAñadir/>} /> 
       </Routes>
