1c4d0f3380d7e57add58d17011a38e597cf17820
Fabián Azuga (Faxter) <azugafabian321@gmail.com>
2024-10-16 14:24:07 -0400

    landing navbar ñam

--- connectway/src/pages/LandingPage.jsx
+++ connectway/src/pages/LandingPage.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LandingPage.css';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt, faChevronDown, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import logo from '../images/logoejemplo.png';
 import portada from '../images/desktop.png';
 
 const LandingPage = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para manejar el menú
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Alternar entre abrir/cerrar menú
+  };
+
   return (
     <div className="landing-container">
       <nav className="nav-landing">
@@ -15,10 +21,16 @@ const LandingPage = () => {
             <img src={logo} alt="Logo" className="nav-logo-image" />
             CONNECTWAY
           </NavLink>
-          <ul className="nav-menu-landing">
+
+          <div className="nav-icon-landing" onClick={toggleMenu}>
+            {/* Alterna entre el ícono de hamburguesa y de cierre */}
+            <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} className="menu-icon-landing" />
+          </div>
+
+          <ul className={menuOpen ? "nav-menu-landing active" : "nav-menu-landing"}>
             <li className='nav-items-landing'>
               <NavLink className="nav-link-landing">
-                 Categorías 
+                Categorías 
                 <FontAwesomeIcon icon={faChevronDown} style={{ marginLeft: '5px' }} />
               </NavLink>
             </li>
@@ -28,9 +40,6 @@ const LandingPage = () => {
                 <FontAwesomeIcon icon={faSignInAlt} style={{ marginLeft: '5px' }} />
               </NavLink>
             </li>
-            {/*<li className='nav-items-landing'>
-              <NavLink className="nav-link-start-landing" to="/Home"> Comenzar </NavLink>
-            </li>*/}
           </ul>
         </div>
       </nav>
