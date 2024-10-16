3097bb8182510fc41ca00ed9b426a0aaf7b3f236
Adriana G <adrianagutierrezmollo@gmail.com>
2024-10-15 20:47:18 -0400

    Informacion Audiolibro

--- connectway/src/pages/audiolibros/AudiolibrosInformacion.jsx
+++ connectway/src/pages/audiolibros/AudiolibrosInformacion.jsx
@@ -4,9 +4,10 @@ import NoContentPage from '../noContent/noContentPage'
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import NavBar from "../../components/PaginaInicio/Navbar";
-
-
-
+import Logo from '../../images/logoejemplo.png';
+import Audifono from '../../images/audifonos.png';
+import Cabeza from '../../images/cabeza.png';
+import Hora from '../../images/hora.png';
 
 const AudiolibrosInformacion = () => {
     const location = useLocation();
@@ -16,16 +17,58 @@ const AudiolibrosInformacion = () => {
         if (idLibro) {
         }
     }, [idLibro]);
+
+    
     return (
         <div className="pagina-inicio">
             <NavBar/>
-            <div className="content">
-            <h1>Información del Audiolibro</h1>
-            {idLibro ? (
-                <p>ID del Audiolibro: {idLibro}</p>
-            ) : (
-                <p>No se ha proporcionado una ID del audiolibro.</p>
-            )}
+            <div className="audiolibro-container">
+            
+                <div className="audiolibro-detalles">
+                    
+                    <div className="contenido-audiolibro p">
+                        <p><strong>Título:</strong> Título del Audiolibro</p>
+
+                        <p><strong>Autor:</strong> Autor del Audiolibro</p>
+                        <p><strong>Descripción:</strong> Aquí va una breve descripción del audiolibro.</p>
+                    </div> 
+                    <div className="audiolibro-portada">
+                        <img src={Logo} alt="Portada del Audiolibro" />
+                    </div>
+                </div>
+                
+                <hr style={{ border: '2px solid black', width: '600px', marginLeft: '120px' }} />
+                
+
+                <div className="detalles-orden" >  
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <p style={{ marginRight: '20px' }}>
+                                <strong>Calificación:</strong> ★★★★☆
+                            </p>
+                            <p>
+                                <img src={Hora} alt="Icono de hora" className="categoria-icono" />
+                                <strong>Minutos:</strong> 120 minutos
+                            </p>
+                    </div>
+                    <div>
+                            <p><strong>Audio:</strong> Disponible</p>
+                    </div>
+                    
+                </div>
+
+                <hr style={{ border: '2px solid black', width: '600px', marginBottom: '10px', marginLeft: '120px' }} />
+                
+                <div>
+                    <button className="btn-reproducir">
+                            <img src={Audifono} alt="Audífono" style={{ width: '20px', marginRight: '10px' }} />
+                                Reproducir
+                    </button>
+                </div>
+        
+                <div className="audiolibro-categoria">
+                    <img src={Cabeza} alt="Audífono" className="categoria-icono" />
+                    <p><strong>Categoría:</strong> Categoría del Audiolibro</p>
+                </div>
             </div>
         </div>
     );
