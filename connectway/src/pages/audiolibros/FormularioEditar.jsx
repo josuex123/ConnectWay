291ff1f3dec2e9ead4b177674078c3653df1d0d5
Josue <josue7pedro@gmail.com>
2024-10-16 22:21:27 -0400

    desactivado guardar

--- connectway/src/pages/audiolibros/FormularioEditar.jsx
+++ connectway/src/pages/audiolibros/FormularioEditar.jsx
@@ -347,6 +347,12 @@ const AudiobookEdit = () => {
                     <button className="cancel-bot" type="button" onClick={() => window.history.back()}>Cancelar</button>
                     <button className="submit-bot" type="button" 
                         onClick={openConfirmModal}
+                        disabled={!isFormValid}
+                        style={{
+                            backgroundColor: isFormValid ? '#2CE080' : '#d3d3d3', 
+                            color: isFormValid ? '#03314B' : '#666', 
+                            cursor: isFormValid ? 'pointer' : 'not-allowed', // Cursor de puntero si habilitado, no permitido si deshabilitado
+                        }}
                        
                     >Guardar
                     </button>
