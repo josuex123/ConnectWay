7ed7b1b61ab016f9ab34aa8e01c5025e842a0520
Josue <josue7pedro@gmail.com>
2024-10-17 23:13:12 -0400

    pagina inicio cambiado

--- connectway/src/components/PaginaInicio/PaginaInicio.jsx
+++ connectway/src/components/PaginaInicio/PaginaInicio.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
 import Description from './Description';
+import '../../estilos/PaginaInicio/inicio.css'
 import NoContentPage from '../../pages/noContent/noContentPage';
 //import MainImage from './MainImage';
 
 const PaginaInicio = () => {
     return (
         <div className="pagina-inicio">
+            <div  className="container">
             <Navbar />
             <Header />
+            </div>
+            
             <div className="content">
                 <Description/>
                 
