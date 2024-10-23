f9597e5cb79c65e4dc81ce00adffc2e4ae5ac02d
rodrivc22 <manfredorvargas84@gmail.com>
2024-10-22 22:48:28 -0400

    nuevos cambios perfil y audiolibros

--- connectway/src/pages/audiolibros/AudiolibrosInformacion.jsx
+++ connectway/src/pages/audiolibros/AudiolibrosInformacion.jsx
@@ -9,6 +9,8 @@ import Cabeza from '../../images/cabeza.png';
 import Hora from '../../images/hora.png';
 import '../../estilos/Audiolibros/AudiolibrosInformacion/AudiolibrosInformacion.css';
 
+
+
 const AudiolibrosInformacion = () => {
     const isDisabled = true; 
     const location = useLocation();
