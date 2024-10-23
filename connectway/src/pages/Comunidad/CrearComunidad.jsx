cb22845d9d994c0b733dd211805097ad18332de4
Fabián Azuga (Faxter) <azugafabian321@gmail.com>
2024-10-23 00:03:11 -0400

    Preparación del terreno de Comunidad

---
+++ connectway/src/pages/Comunidad/CrearComunidad.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CrearComunidad = () => {
+  return (
+    <div>
+      <h1>Crear Comunidad</h1>
+      {/*Aquí va EL FORMULARIO DE CREAR COMUNIDAD*/}
+    </div>
+  );
+};
+
+export default CrearComunidad;
---
+++ connectway/src/pages/Comunidad/UnirseComunidad.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UnirseComunidad = () => {
+  return (
+    <div>
+      <h1>Unirse a Comunidad</h1>
+     
+    </div>
+  );
+};
+
+export default UnirseComunidad;
