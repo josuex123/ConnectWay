77b19ebbcdc17b7044f23010d38c5417ee270c77
Adriana G <adrianagutierrezmollo@gmail.com>
2024-11-07 15:50:23 -0400

    Diseño Landing Page mejorado

--- connectway/src/pages/LandingPage.jsx
+++ connectway/src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt, faChevronDown, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import logo from '../images/logoejemplo.png';
-import portada from '../images/ji.jpeg';
+import portada from '../images/online-education2.png';
 
 
 const LandingPage = () => {
