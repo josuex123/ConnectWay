b13c0e7e5673317af55f6b4b4ecf298e0bf07938
GustavoLuizaga <luizagamerinogustavo1@gmail.com>
2024-11-14 21:26:36 -0400

    Crear comunidad e inicar coleccion miembros

--- connectway/src/pages/Comunidad/CrearComunidad.jsx
+++ connectway/src/pages/Comunidad/CrearComunidad.jsx
@@ -66,8 +66,15 @@ function FormularioCrearComunidad() {
     try {
       const imagenUrl = await subirImagenYObtenerUrl(imageFiles[0]);
       const comunidadDoc = { titulo, descripcion, imagenURL: imagenUrl };
+    
+      // Crear el documento en la colección "comunidades" dentro de la categoría especificada
       const categoriaRef = doc(db, "Comunidades", categoria);
-      await addDoc(collection(categoriaRef, "comunidades"), comunidadDoc);
+      const comunidadRef = await addDoc(collection(categoriaRef, "comunidades"), comunidadDoc);
+    
+      // Añadir un documento temporal vacío en la subcolección "miembros"
+      await addDoc(collection(comunidadRef, "miembros"), {
+        temporal: true // Esto es solo para que la subcolección exista
+      });
 
       setTitulo('');
       setCategoria('');
