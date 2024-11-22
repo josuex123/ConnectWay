89b451e612e444d16c84e551d561b65d3fdddf72
Adriana G <adrianagutierrezmollo@gmail.com>
2024-11-22 07:19:53 -0400

    Mejora diseño Inicio de sesion

--- connectway/src/pages/SesionUsuario/CrearCuenta.jsx
+++ connectway/src/pages/SesionUsuario/CrearCuenta.jsx
@@ -1,236 +1,284 @@
-import React,{useState} from 'react';
-import '../../estilos/SesionUsuario/CrearCuenta.css';
-import { useNavigate } from 'react-router-dom';
-import registrarUsuario from '../../Services/UsuarioServicios/RegistrarUsuarioCorreoContraseña';
-import { guardarUsuario } from '../../Services/UsuarioServicios/GuardarUsuario';
-import { verificarNombreUsuarioExistente } from '../../Services/UsuarioServicios/VerificarNombreUsuarioExistente';
-import ModalCargando from '../../components/Modal/ModalCargando'; 
+import React, { useState } from "react";
+import "../../estilos/SesionUsuario/CrearCuenta.css";
+import { useNavigate } from "react-router-dom";
+import registrarUsuario from "../../Services/UsuarioServicios/RegistrarUsuarioCorreoContraseña";
+import { guardarUsuario } from "../../Services/UsuarioServicios/GuardarUsuario";
+import { verificarNombreUsuarioExistente } from "../../Services/UsuarioServicios/VerificarNombreUsuarioExistente";
+import ModalCargando from "../../components/Modal/ModalCargando";
 
 const CrearCuenta = () => {
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [confirmPasswordError, setConfirmPasswordError] = useState('');
-    const [user, setUser] = useState('');
-    const [userError, setUserError] = useState(''); // Estado para el error del usuario
-    const [isLoading, setIsLoading] = useState(false);  // Estado de carga
-    const navigate = useNavigate();
- 
-    const togglePasswordVisibility = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [user, setUser] = useState("");
+  const [userError, setUserError] = useState(""); // Estado para el error del usuario
+  const [isLoading, setIsLoading] = useState(false); // Estado de carga
+  const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
-    };
+  };
 
-    const handleEmailChange = (e) => {
-      const value = e.target.value;
-      setEmail(value);
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
 
-      // Arreglar Jere el value .length<20 xd, cambiar si sera aparte de @gmail.com
-      if(value === '' || value.length < 20){
-          setEmailError(''); 
-      }else if(!value.includes('@gmail.com')){
-          setEmailError('El correo es inválido');
-      }else{
-          setEmailError('');
-      }
+    // Arreglar Jere el value .length<20 xd, cambiar si sera aparte de @gmail.com
+    if (value === "" || value.length < 20) {
+      setEmailError("");
+    } else if (!value.includes("@gmail.com")) {
+      setEmailError("El correo es inválido");
+    } else {
+      setEmailError("");
+    }
   };
 
   const handleEmailBlur = () => {
-      if(email !== '' && !email.includes('@gmail.com')){
-          setEmailError('El correo es inválido');
-      }
+    if (email !== "" && !email.includes("@gmail.com")) {
+      setEmailError("El correo es inválido");
+    }
   };
 
-  const handlePasswordChange = (e) =>{
+  const handlePasswordChange = (e) => {
     const value = e.target.value;
     setPassword(value);
 
-    let error = '';
-    if(!/[0-9]/.test(value)){
-      error = 'La contraseña debe contener al menos un número.';
-        } else if (!/[A-Z]/.test(value)) {
-            error = 'La contraseña debe contener al menos una letra mayúscula.';
-        } else if (!/[a-z]/.test(value)) {
-            error = 'La contraseña debe contener al menos una letra minúscula.';
-        } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
-            error = 'La contraseña debe contener al menos un carácter especial.';
-        }
+    let error = "";
+    if (!/[0-9]/.test(value)) {
+      error = "La contraseña debe contener al menos un número.";
+    } else if (!/[A-Z]/.test(value)) {
+      error = "La contraseña debe contener al menos una letra mayúscula.";
+    } else if (!/[a-z]/.test(value)) {
+      error = "La contraseña debe contener al menos una letra minúscula.";
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+      error = "La contraseña debe contener al menos un carácter especial.";
+    }
     setPasswordError(error);
-  }
+  };
   const handleConfirmPasswordChange = (e) => {
     setConfirmPassword(e.target.value);
   };
 
   const handleUserNameChange = async (e) => {
     let value = e.target.value;
-  
+
     // Elimina los espacios finales en tiempo real
     value = value.trimEnd();
-  
+
     setUser(value);
-  
+
     // Verificación en tiempo real
-    if (value.trim() !== '') {
+    if (value.trim() !== "") {
       const usuarioExistente = await verificarNombreUsuarioExistente(value);
       if (usuarioExistente) {
-        setUserError('El nombre de usuario ya existe. Intenta con otro.');
+        setUserError("El nombre de usuario ya existe. Intenta con otro.");
       } else {
-        setUserError('');
+        setUserError("");
       }
     } else {
-      setUserError('');
+      setUserError("");
     }
   };
-  
 
   const validateForm = () => {
     let valid = true;
-  
+
     if (!email || emailError) valid = false;
     if (!password || passwordError) valid = false;
-  
+
     if (password !== confirmPassword) {
-      setConfirmPasswordError('Las contraseñas no coinciden.');
+      setConfirmPasswordError("Las contraseñas no coinciden.");
       valid = false;
     } else {
-      setConfirmPasswordError('');
+      setConfirmPasswordError("");
     }
-  
+
     return valid;
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (!validateForm()) {
-      console.log('Por favor, corrige los errores antes de enviar.');
+      console.log("Por favor, corrige los errores antes de enviar.");
       return;
     }
-  
-    if (userError!='') {
-      alert('Por favor, corrige los errores en el campo "Usuario" antes de enviar.');
+
+    if (userError != "") {
+      alert(
+        'Por favor, corrige los errores en el campo "Usuario" antes de enviar.'
+      );
       return;
     }
-  
 
     setIsLoading(true); // modal de carga o algo
     try {
       const response = await registrarUsuario(email, password);
-  
+
       if (response.email) {
         const guardarUsuarioPromise = guardarUsuario(response.email, user);
-        navigate('/IniciarSesion'); // Navegación inmediata
+        navigate("/IniciarSesion"); // Navegación inmediata
         await guardarUsuarioPromise; // Espera solo si es esencial
       } else if (response.error) {
-        alert(response.error);//Reemplazar con un modal
+        alert(response.error); //Reemplazar con un modal
       }
     } catch (error) {
-      console.error('Error al procesar el formulario:', error);
+      console.error("Error al procesar el formulario:", error);
     } finally {
       setIsLoading(false);
     }
   };
-  
+
   return (
     <>
       <div className="register-container">
-        <h1 className="welcome-message">Bienvenido a 
-            <span className="logo-text first-word">Connect</span>
-            <span className="logo-text second-word">Way</span>
-        </h1>
-        
-        <div className="register-box">
-          <h2>Crea una cuenta</h2>
-          <form onSubmit={handleSubmit}>
-            <label>Nombre completo<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <input type="text" placeholder="Ingrese su nombre completo" required />
+        <div className="login-content">
+          <div className="login-box">
+            <h1 className="welcome-message">
+              Bienvenido a<span className="logo-text first-word">Connect</span>
+              <span className="logo-text second-word">Way</span>
+            </h1>
 
-            <label>Usuario<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <input 
-              type="text" 
-              placeholder="Ingrese un usuario" 
-              value={user}
-              onChange={handleUserNameChange}
-              required
+            <div className="register-box">
+              <h2>Crea una cuenta</h2>
+              <form onSubmit={handleSubmit}>
+                <label>
+                  Nombre completo
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Ingrese su nombre completo"
+                  required
+                />
+
+                <label>
+                  Usuario
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Ingrese un usuario"
+                  value={user}
+                  onChange={handleUserNameChange}
+                  required
+                />
+                {userError && <p className="error-message">{userError}</p>}
+
+                <label>
+                  Correo electrónico
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Ingrese su correo electrónico"
+                  value={email}
+                  onChange={handleEmailChange}
+                  onBlur={handleEmailBlur}
+                  autoComplete="email"
+                  required
+                />
+                {emailError && <p className="error-message">{emailError}</p>}
+
+                <label>
+                  Contraseña
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <div className="password-field">
+                  <input
+                    type={isPasswordVisible ? "text" : "password"}
+                    placeholder="Ingrese su contraseña"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                  <img
+                    src={
+                      isPasswordVisible
+                        ? require("../../images/ojo2.png")
+                        : require("../../images/ojo1.png")
+                    }
+                    alt="Mostrar/ocultar contraseña"
+                    className="editIcon"
+                    onClick={togglePasswordVisibility}
+                    style={{ cursor: "pointer", marginTop: "-15px" }}
+                  />
+                </div>
+
+                {passwordError && (
+                  <p className="error-message">{passwordError}</p>
+                )}
+
+                <label>
+                  Confirmar contraseña
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <div className="password-field">
+                  <input
+                    type={isPasswordVisible ? "text" : "password"}
+                    placeholder="Confirme su contraseña"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    required
+                  />
+
+                  <img
+                    src={
+                      isPasswordVisible
+                        ? require("../../images/ojo2.png")
+                        : require("../../images/ojo1.png")
+                    }
+                    alt="Mostrar/ocultar contraseña"
+                    className="editIcon"
+                    onClick={togglePasswordVisibility}
+                    style={{ cursor: "pointer", marginTop: "-15px" }}
+                  />
+                </div>
+
+                {confirmPasswordError && (
+                  <p className="error-message">{confirmPasswordError}</p>
+                )}
+
+                <div className="terms">
+                  <input type="checkbox" required />
+                  <label className="terms-label1">
+                    He leído y acepto los{" "}
+                    <a href="/terms">Términos y Condiciones</a>
+                  </label>
+                </div>
+
+                <button type="submit" className="register-button">
+                  Registrarse
+                </button>
+              </form>
+              <div className="crear-of-ini">
+                <p>
+                  ¿Ya tienes una cuenta?{" "}
+                  <a href="/IniciarSesion" className="create-login1">
+                    Iniciar Sesión
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="login-image">
+            <img
+              src={require("../../images/imagenInicio1.png")}
+              alt="Login Visual"
             />
-            {userError && <p className="error-message">{userError}</p>}
-
-            <label>Correo electrónico<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <input 
-              type="email" 
-              placeholder="Ingrese su correo electrónico" 
-              value={email}
-              onChange={handleEmailChange}
-              onBlur={handleEmailBlur}
-              autoComplete="email"
-              required 
-            />
-            {emailError && <p className="error-message">{emailError}</p>}
-
-
-            <label>Contraseña<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <div className="password-field">
-              <input 
-                type={isPasswordVisible ? "text" : "password"} 
-                placeholder="Ingrese su contraseña" 
-                value={password}
-                onChange={handlePasswordChange}
-                required 
-              />
-              <img 
-                src={isPasswordVisible ? require('../../images/ojo2.png') : require('../../images/ojo1.png')} 
-                alt="Mostrar/ocultar contraseña" 
-                className="editIcon" 
-                onClick={togglePasswordVisibility} 
-                style={{ cursor: 'pointer',marginTop: '-15px' }}
-              />
-            </div>
-
-            {passwordError && <p className="error-message">{passwordError}</p>}
-
-            <label>Confirmar contraseña<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <div className="password-field">
-              <input 
-                type={isPasswordVisible ? "text" : "password"} 
-                placeholder="Confirme su contraseña" 
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-                required 
-              />
-
-              <img 
-                src={isPasswordVisible ? require('../../images/ojo2.png') : require('../../images/ojo1.png')} 
-                alt="Mostrar/ocultar contraseña" 
-                className="editIcon" 
-                onClick={togglePasswordVisibility} 
-                style={{ cursor: 'pointer',marginTop: '-15px' }}
-              />
-            </div>
-
-            {confirmPasswordError && <p className="error-message">{confirmPasswordError}</p>}
-            
-            <div className="terms">
-              <input type="checkbox" required />
-              <label className="terms-label1" >He leído y acepto los <a href="/terms">Términos y Condiciones</a></label>
-            </div>
-
-            <button type="submit" className="register-button">
-              Registrarse
-            </button>
-          </form>
-          <div className='crear-of-ini'>
-              <p>¿Ya tienes una cuenta? <a href="/IniciarSesion" className="create-login1">Iniciar Sesión</a></p>
           </div>
         </div>
       </div>
       <ModalCargando
-        isOpen={isLoading} 
+        isOpen={isLoading}
         onClose={() => {}}
         type="loading"
         message="Cargando, por favor espera...\n "
-        iconClass="fa fa-spinner fa-spin" 
+        iconClass="fa fa-spinner fa-spin"
       />
     </>
   );
--- connectway/src/pages/SesionUsuario/IniciarSesion.jsx
+++ connectway/src/pages/SesionUsuario/IniciarSesion.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import '../../estilos/SesionUsuario/IniciarSesion.css';
-import { useNavigate } from 'react-router-dom';
-import authService from '../../Services/UsuarioServicios/VerificarUsuario';
-import { obtenerNombreUsuario } from '../../Services/UsuarioServicios/NombreUsuarioPorIdDoc';
-import ModalCargando from '../../components/Modal/ModalCargando'; 
+import React, { useState } from "react";
+import "../../estilos/SesionUsuario/IniciarSesion.css";
+import { useNavigate } from "react-router-dom";
+import authService from "../../Services/UsuarioServicios/VerificarUsuario";
+import { obtenerNombreUsuario } from "../../Services/UsuarioServicios/NombreUsuarioPorIdDoc";
+import ModalCargando from "../../components/Modal/ModalCargando";
 
 const IniciarSesion = () => {
-  const [emailOrUsername, setEmailOrUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [emailOrUsername, setEmailOrUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);  // Estado de carga
+  const [isLoading, setIsLoading] = useState(false); // Estado de carga
   const navigate = useNavigate();
-  
+
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
@@ -24,18 +24,18 @@ const IniciarSesion = () => {
     setEmailOrUsername(value);
 
     // Arreglar Jere el value .length<20 xd, cambiar si sera aparte de @gmail.com
-    if(value === '' || value.length < 20){
-        setEmailError(''); 
-    }else if(!value.includes('@gmail.com')){
-        setEmailError('El correo es inválido');
-    }else{
-        setEmailError('');
+    if (value === "" || value.length < 20) {
+      setEmailError("");
+    } else if (!value.includes("@gmail.com")) {
+      setEmailError("El correo es inválido");
+    } else {
+      setEmailError("");
     }
   };
 
   const handleEmailBlur = () => {
-    if(emailOrUsername !== '' && !emailOrUsername.includes('@gmail.com')){
-        setEmailError('El correo es inválido');
+    if (emailOrUsername !== "" && !emailOrUsername.includes("@gmail.com")) {
+      setEmailError("El correo es inválido");
     }
   };
 
@@ -45,21 +45,21 @@ const IniciarSesion = () => {
     setIsLoading(true);
     try {
       const user = await authService.signInWithEmail(emailOrUsername, password);
-      console.log('Usuario autenticado:', user);
+      console.log("Usuario autenticado:", user);
 
       // Guardar el correo en sessionStorage
-      sessionStorage.setItem('correoUsuario', emailOrUsername);
+      sessionStorage.setItem("correoUsuario", emailOrUsername);
 
       const username = await obtenerNombreUsuario(emailOrUsername);
 
-      sessionStorage.setItem('nombreUsuario', username);
+      sessionStorage.setItem("nombreUsuario", username);
 
-      navigate('/Home/0');
+      navigate("/Home/0");
     } catch (error) {
       setIsLoading(false);
-      
-      console.error('Error en inicio de sesión:', error.message);
-      alert('Error al iniciar sesión: ' + error.message);
+
+      console.error("Error en inicio de sesión:", error.message);
+      alert("Error al iniciar sesión: " + error.message);
     } finally {
       setIsLoading(false);
     }
@@ -69,80 +69,113 @@ const IniciarSesion = () => {
   const handleGoogleSignIn = async () => {
     try {
       const user = await authService.signInWithGoogle();
-      console.log('Usuario autenticado con Google:', user);
+      console.log("Usuario autenticado con Google:", user);
 
       // Guardar el correo en sessionStorage
-      sessionStorage.setItem('correoUsuario', user.email);
+      sessionStorage.setItem("correoUsuario", user.email);
 
-      navigate('/Home/0');
+      navigate("/Home/0");
     } catch (error) {
-      console.error('Error en inicio de sesión con Google:', error.message);
-      alert('Error al iniciar sesión con Google: ' + error.message);
+      console.error("Error en inicio de sesión con Google:", error.message);
+      alert("Error al iniciar sesión con Google: " + error.message);
     }
   };
 
   return (
     <>
       <div className="login-container">
-        <h1 className="welcome-message">
-          Bienvenido a 
-          <span className="logo-text first-word">Connect</span>
-          <span className="logo-text second-word">Way</span>
-        </h1>
-        
-        <div className="login-box">
-          <h2>Iniciar Sesión</h2>
-          <form onSubmit={handleEmailSignIn}>
-            <label>Correo electrónico<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <input 
-              type="email" 
-              placeholder="Ingrese su correo electrónico" 
-              value={emailOrUsername}
-              onChange={handleEmailChange}
-              onBlur={handleEmailBlur}
-              autoComplete="email"
-              required 
-            />
-            {emailError && <p className="error-message">{emailError}</p>}
+        <div className="login-content">
+          <div className="login-box">
+            <h1 className="welcome-message">
+              Bienvenido a<span className="logo-text first-word">Connect</span>
+              <span className="logo-text second-word">Way</span>
+            </h1>
+            <div className="login-box2">
+              <h2>Iniciar Sesión</h2>
+              <form onSubmit={handleEmailSignIn}>
+                <label>
+                  Correo electrónico
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Ingrese su correo electrónico"
+                  value={emailOrUsername}
+                  onChange={handleEmailChange}
+                  onBlur={handleEmailBlur}
+                  autoComplete="email"
+                  required
+                />
+                {emailError && <p className="error-message">{emailError}</p>}
 
-            <label>Contraseña<span style={{ color: 'red', marginLeft: '2px' }}>*</span></label>
-            <div className="password-field">
-              <input 
-                type={isPasswordVisible ? "text" : "password"} 
-                placeholder="Ingrese su contraseña" 
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required 
-              />
-              <img 
-                src={isPasswordVisible ? require('../../images/ojo2.png') : require('../../images/ojo1.png')} 
-                alt="Mostrar/ocultar contraseña" 
-                className="editIcon" 
-                onClick={togglePasswordVisibility} 
-                style={{ cursor: 'pointer', marginTop: '-15px' }}
-              />
+                <label>
+                  Contraseña
+                  <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+                </label>
+                <div className="password-field">
+                  <input
+                    type={isPasswordVisible ? "text" : "password"}
+                    placeholder="Ingrese su contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <img
+                    src={
+                      isPasswordVisible
+                        ? require("../../images/ojo2.png")
+                        : require("../../images/ojo1.png")
+                    }
+                    alt="Mostrar/ocultar contraseña"
+                    className="editIcon"
+                    onClick={togglePasswordVisibility}
+                    style={{ cursor: "pointer", marginTop: "-15px" }}
+                  />
+                </div>
+                {passwordError && (
+                  <p className="error-message">{passwordError}</p>
+                )}
+
+                <a href="/RecuperarContrasenia" className="forgot-password1">
+                  ¿Olvidaste tu contraseña?
+                </a>
+
+                <button type="submit" className="login-button">
+                  Iniciar Sesión
+                </button>
+              </form>
+
+              <p>
+                ¿No tienes una cuenta?{" "}
+                <a href="/CrearCuenta" className="create-login1">
+                  Crea una
+                </a>
+              </p>
+              <p>O</p>
+              <button className="google-button1" onClick={handleGoogleSignIn}>
+                <img
+                  src={require("../../images/IconGo.png")}
+                  alt="Google Icon"
+                  className="editIcon"
+                />
+                Continuar con Google
+              </button>
             </div>
-            {passwordError && <p className="error-message">{passwordError}</p>}
-
-            <a href="/RecuperarContrasenia" className="forgot-password1">¿Olvidaste tu contraseña?</a>
-
-            <button type="submit" className="login-button">Iniciar Sesión</button>
-          </form>
-
-          <p>¿No tienes una cuenta? <a href="/CrearCuenta" className="create-login1">Crea una</a></p>
-          <p>O</p>
-          <button className="google-button1" onClick={handleGoogleSignIn}>
-            <img src={require('../../images/IconGo.png')} alt="Google Icon" className='editIcon' />
-            Continuar con Google
-          </button>
+          </div>
+          <div className="login-image">
+            <img
+              src={require("../../images/imagenInicio1.png")}
+              alt="Login Visual"
+            />
+          </div>
         </div>
       </div>
       <ModalCargando
-        isOpen={isLoading} 
+        isOpen={isLoading}
         onClose={() => {}}
         type="loading"
         message="Cargando, por favor espera...\n "
-        iconClass="fa fa-spinner fa-spin" 
+        iconClass="fa fa-spinner fa-spin"
       />
     </>
   );
